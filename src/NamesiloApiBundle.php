372b86cc27157ff432140c151bee7e8e51762a72
Julio Rodriguez <julio@estratos.net>
2024-09-25 07:58:20 -0600

    fist commit

---
+++ src/NamesiloApiBundle.php
@@ -0,0 +1,10 @@
+<?php
+// src/AcmeBlogBundle.php
+namespace Estratos\NamesiloApiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class NamesiloApiBundle extends AbstractBundle
+{
+
+}
---
+++ src/Service/RequestService.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Estratos\NamesiloApiBundle;
+
+
+
+class RequestService
+{
+
+    private $endpoint;
+    private $metodo;
+    private $json;
+    private $token;
+    private $bearer;
+
+    public function __construct( $endpoint, $metodo = null, $json = null, $token = null, $bearer = false)
+
+    {
+        $this->endpoint = $endpoint;
+        $this->metodo = $metodo;
+        $this->json = $json;
+        $this->token = $token;
+        $this->bearer = $bearer;
+
+    }
+
+    public function RequestApi() {
+        $ch = curl_init($this->endpoint);
+        if (isset($this->metodo))    {
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $this->metodo);    
+        }
+        if (isset($this->json)) {
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $this->json);
+        }
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+        if (isset( $this->token ) and ($this->bearer == false  )) {      
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Content-Length: ' . strlen($this->json), 'x-auth: ' . $this->token));
+        }
+        if (isset( $this->token)  and ( $this->bearer == true ))  {
+            //Set your auth headers
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+            'Authorization: Bearer ' . $this->token
+        ));
+
+        curl_setopt($ch, CURLOPT_URL, $this->endpoint);
+
+        }
+        curl_setopt($ch, CURLOPT_TIMEOUT, 20);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return $result;
+    }
+}
