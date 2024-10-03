e65d677a83e609985cb8170f0319ea977c718505
Julio Rodriguez <julio@estratos.net>
2024-10-02 18:12:35 -0600

    more functions

--- src/NamesiloApi.php
+++ src/NamesiloApi.php
@@ -17,12 +17,12 @@ class NamesiloApi extends AbstractBundle
 
     public function getPrices()
     {
-        $params = '?'.'version=1'.'&'.'type=json'.'&'.'key='.$this->tokenkey;
+
         $endpoint = self::BASE_URL . 'getPrices';
-        $endpoint = $endpoint . $params;
-       
+        $endpoint = $endpoint . $this->getParams();
+
         ///// https://www.namesilo.com/api/getPrices?version=1&type=xml&key=12345
-        $service = new RequestService($endpoint, null , null, null, false);
+        $service = new RequestService($endpoint, null, null, null, false);
 
 
         return $service->RequestApi();
@@ -30,10 +30,9 @@ class NamesiloApi extends AbstractBundle
 
     public function registerDomain()
     {
-        $params = '?'.'version=1'.'&'.'type=json'.'&'.'key='.$this->tokenkey;
+
         $endpoint = self::BASE_URL . 'registerDomain';
-        $endpoint = $endpoint . $params;
-        $json = json_encode(['version' => '1', 'type' => 'json', 'key' => $this->tokenkey]);
+        $endpoint = $endpoint . $this->getParams();
 
         //// https://www.namesilo.com/api/registerDomain?version=1&type=xml&key=12345&domain=namesilo.com&years=2&private=1&auto_renew=1
         $service = new RequestService($endpoint, null, null, null, false);
@@ -44,17 +43,67 @@ class NamesiloApi extends AbstractBundle
 
     public function renewDomain()
 
-    {   
-        $params = '?'.'version=1'.'&'.'type=json'.'&'.'key='.$this->tokenkey;
+    {
         $endpoint = self::BASE_URL . 'renewDomain';
-        $endpoint = $endpoint . $params;
-        
+        $endpoint = $endpoint . $this->getParams();
+
         //// https://www.namesilo.com/api/renewDomain?version=1&type=xml&key=12345&domain=namesilo.com&years=2
         $service = new RequestService($endpoint, null, null, null, false);
 
         return $service->RequestApi();
     }
-    
+
+
+    public function listDomains()
+    {
+
+
+        $endpoint = self::BASE_URL . 'listDomains';
+        $endpoint = $endpoint . $this->getParams();
+        // https://www.namesilo.com/api/listDomains?version=1&type=xml&key=12345&withBid=1&pageSize=10
+
+        $service = new RequestService($endpoint, null, null, null, false);
+
+        return $service->RequestApi();
+    }
+
+    public function getAccountBalance()
+    {
+
+        $endpoint = self::BASE_URL . 'getAccountBalance';
+        $endpoint = $endpoint . $this->getParams();
+        //https: //www.namesilo.com/api/getAccountBalance?version=1&type=xml&key=12345
+
+        $service = new RequestService($endpoint);
+
+        return $service->RequestApi();
+    }
+
+
+    public function addAccountFunds(float $amount, int $paymentid)
+    {
+
+        /// validate amount
+        if (!is_float($amount)) return 'amount must be float type';
+        /// we may limit to 2 digit precision
+
+        $endpoint = self::BASE_URL . 'addAccountFunds';
+        $endpoint = $endpoint . $this->getParams() . '&amount=' . (string)$amount . '&paiment_id' . (string)$paymentid;
+        //  https://www.namesilo.com/api/addAccountFunds?version=1&type=xml&key=12345&amount=65.43&payment_id=123
+
+
+        $service = new RequestService($endpoint);
+
+        return $service->RequestApi();
+    }
+
+
+    private function getParams()
+    {
+
+        return '?' . 'version=1' . '&' . 'type=json' . '&' . 'key=' . $this->tokenkey;
+    }
+
     /**
      * Set the value of tokenkey
      *
